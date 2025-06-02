6c8ce26d0d92a6848663842786e2470f1975d083
RoxasMelvino <melvinoroxasbvt2024@gmail.com>
2025-06-02 11:04:30 -0700

    add logic for projects page

--- pages/projects.js
+++ pages/projects.js
@@ -1,3 +1,5 @@
+import renderProjects from "../scripts/portfolio.js";
+
 export default function projects() {
     document.querySelector(".projects-section").innerHTML = `
         <header><h1>Projects</h1></header>
@@ -9,5 +11,13 @@ export default function projects() {
             <button class="close-category-btn">X</button>
             <!-- <button class="category__item"></button> -->
         </article>
+        
+        <div class="modal display-none">
+            <button class="modal--close">X</button>
+            <!-- <header><h2 class="modal__proj-name"></h2></header>
+            <a href="" > <p>Go to project</p> </a> -->
+        </div>    
     `;
+    
+    renderProjects();
 }
